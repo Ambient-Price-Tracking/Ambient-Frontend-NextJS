7ad8e383f7bce9b5f79edaf58ea2ce0261d69a78
HeetAnkola <67467323+HeetAnkola@users.noreply.github.com>
2022-11-14 17:54:34 +0530

    back ground updated (#6)
    * back ground updated
    
    * updated index.js
    
    Co-authored-by: Heet kumar Kothadiya <78725162+heet-kumar@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,8 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <>
-      <h1 className="heading">Hello World!!!</h1>
+      <h1 className="heading">Hello World</h1>
+      <h1 className="heading">Ambient</h1>
     </>
   )
 }
