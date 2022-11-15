19c7e466c62a0f9fbffddff46ccf3a87cf0cd451
HeetAnkola <heet.ankola2019@vitbhopal.ac.in>
2022-11-15 22:51:16 +0530

    Not major update
    just started work

---
+++ components/HomeScreen/HomeScreen.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+import styles from '../../styles/HomeScreen.module.css'
+
+const HomeScreen = () => {
+    return (
+        <div className={styles.container}>
+        <span className={styles.title}>Life Is Short And 
+        The World Is Wide!</span>
+        </div>
+    )
+
+}
+
+export default HomeScreen
--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,8 +1,25 @@
+import styles from '../../styles/Navbar.module.css'
+import Image from 'next/image'
 const Navbar = () => {
     return(
-        <div>
-            <h1>This is from Navbar</h1>
+        <nav className={styles.navbar}>
+        <div className={styles.navbar__logo}>
+          {/* <Image src='../../public/logo.png' alt="logo" width={200} height={40} /> */}
         </div>
+        <div className={styles.navbar__links}>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
 }
 
